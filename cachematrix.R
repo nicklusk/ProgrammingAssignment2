ce2e08d2f7892127f66f8d8652087bc1c86958a8
nlusk <luskmonster@gmail.com>
2014-10-25 13:54:26 -0400

    Completed file cachematrix.R for R Programming class at Coursera. Programming Assignment 2 for peer review.

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for making and manipulating inverted matrices with caching
 
-## Write a short comment describing this function
+## Creates cacheable matrix for inputting to
+## cacheSolve() function which sets and gets 
+## the cached values
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix.one = matrix()) {
+        
+        # Do I have a matrix?
+        if (!is.matrix(matrix.one)) {
+                stop("You don't have a matrix")
+        }
+        ## Let's initialize the inverse
+        matrix.inv <- NULL
+        ## Let's set the matrix
+        set <- function(y) {
+                matrix.one <<- y
+                matrix.inv <<- NULL
+        }
+        
+        # Let's grab and set the cached inverse of the  matrix using solve()
+        get <- function() matrix.one
+        set.inv <- function(solve) matrix.inv <<- solve
+        get.inv <- function() matrix.inv
+        ## Let's return a list of all the methods we just made
+        list(
+                set = set, 
+                get = get,
+                set.inv = set.inv,
+                get.inv = get.inv)  
 }
 
+## Computes the inverse of the matrix returned by makeCacheMatrix()
+## If the inverse has not already been set it sets it for you
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix.cached, ...) {
+        matrix.inv <- matrix.cached$get.inv()
+        # Do I have a matrix?
+        if(!is.null(matrix.inv)) {
+                message("Grabbing matrix cache inverse")
+                return(matrix.inv)
+        }
+        # Let's create an inverted matrix
+        matrix.two <- matrix.cached$get()
+        matrix.inv <- solve(matrix.two)
+        matrix.cached$set.inv(matrix.inv)
+        matrix.inv 
 }
-
-
-
-
